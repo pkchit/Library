10df7521c7d41c964586a7549f82a4ffbdfe84d0
pkchit <puneet.chitkariya@gmail.com>
2019-12-12 21:49:13 +0530

    Second commit

--- app/src/main/java/com/example/library/MainActivity.java
+++ app/src/main/java/com/example/library/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.library;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button loginIn, signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginIn= (Button)findViewById(R.id.main_login);
+        signUp = (Button)findViewById(R.id.main_signup);
     }
 }
